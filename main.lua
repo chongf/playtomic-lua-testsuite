733e357552e1837bac69c281e1ce8dfcfdefa458
Ben Chong <chongf@purdue.edu>
2012-01-03 12:58:12 -0300

    data api

--- main.lua
+++ main.lua
@@ -7,10 +7,30 @@ analytics.init(5751,"5a80457160d84193","6f1474a2afce4583a612c2c8c1c8ce")
 
 
 -- attach events to buttons
-button.createButton("Load Game Vars",40,50,250,50,playtomicTestSuite.LoadGameVars)
-button.createButton("Submit Scores (Simple)",40,120,250,50,playtomicTestSuite.SubmitScoreSimple)
-button.createButton("Submit Scores (Advanced)",40,190,250,50,playtomicTestSuite.SubmitScoreAdvanced)
-button.createButton("Save Player Level",40,260,250,50,playtomicTestSuite.SavePlayerLevel)
+
+-- BASIC
+button.createButton("Basic: CustomMetric",40,150,250,20,playtomicTestSuite.CustomMetric)
+button.createButton("Basic: LevelCounterMetric",40,180,250,20,playtomicTestSuite.LevelCounterMetric)
+button.createButton("Basic: LevelAverageMetric",40,210,250,20,playtomicTestSuite.LevelAverageMetric)
+button.createButton("Basic: LevelRangedMetric",40,240,250,20,playtomicTestSuite.LevelRangedMetric)
+button.createButton("Basic: HeatMap",40,270,250,20,playtomicTestSuite.HeatMap)
+
+-- ADVANCED
+	-- DATA METRIC ( NOTE: you need to enable Data API in the Playtomic Dashboard. Settings-> Game Details -> Data API)
+	button.createButton("Data API: CustomMetric",40,300,250,20,playtomicTestSuite.DataMetricsLoadCustomMetric)
+	button.createButton("Data API: LevelCounterMetric",40,330,250,20,playtomicTestSuite.DataMetricsLoadLevelCounterMetric)	
+	-- more to come
+		
+	-- GAMEVARS
+	button.createButton("Load Game Vars",40,30,250,20,playtomicTestSuite.LoadGameVars)
+
+	-- LEVEL SHARING
+	button.createButton("Save Player Level",40,120,250,20,playtomicTestSuite.SavePlayerLevel)
+	
+	-- LEADERBOARDS
+	button.createButton("Submit Scores (Simple)",40,60,250,20,playtomicTestSuite.SubmitScoreSimple)
+	button.createButton("Submit Scores (Advanced)",40,90,250,20,playtomicTestSuite.SubmitScoreAdvanced)
+
 
 
 -- tests
--- playtomic-tests.lua
+++ playtomic-tests.lua
@@ -11,14 +11,128 @@
 local TEST = {}
 	
 	-- CUSTOM METRICS, LEVEL METRICS, AND HEATMAPS
-	-- tried and tested in Ghost vs Monsters sample
+	-- tried and tested in Blast Monkeys, a top iOS+Android Game -> https://market.android.com/details?id=com.yobonja.blastmonkeysfree&hl=en	
+	-- tried and tested in Ghost vs Monsters sample. To see implementation -> https://github.com/chongf/Ghost-vs-Monsters-Playtomic
 	
-	-- DATA METRICS
+	-- CUSTOM METRIC
+	TEST.CustomMetric = function()
+		local eventData = {
+			type = "custom",
+			eventGroup = "MainMenu"
+		}
+
+		analytics.logEvent("SomeEventHappened",eventData);
+		analytics.forceSend();		
+	end
 	
-	
+	-- HEATMAP
+	TEST.HeatMap = function()
+		local eventData = {
+			type = "heatmap",
+			mapName = "level1",
+			x = math.random(100),
+			y = math.random(100),
+		}
+
+		analytics.logEvent("deathposition",eventData); -- eg: we track positions where players get killed
+		analytics.forceSend();	
+	end
+		
+	-- LEVEL METRICS
+		-- LEVEL COUNTER METRIC
+		TEST.LevelCounterMetric = function()
+			eventData = {
+				type = "counter",
+				levelName = "level1",
+
+			}
+
+			analytics.logEvent("Win",eventData); -- eg: we log wins on level1
+			analytics.forceSend();		
+		end
+
+		-- LEVEL AVERAGE METRIC
+		TEST.LevelAverageMetric = function()
+			eventData = {
+				type = "average",
+				levelName = "level1",
+				value = math.random(10000) + 1000, -- randomized for fun
+
+			}
+
+			analytics.logEvent("AverageTime",eventData); -- eg: we track average time to complete level1
+			analytics.forceSend();	
+		end
+
+		-- LEVEL RANGED METRIC
+		TEST.LevelRangedMetric = function()
+			eventData = {
+				type = "ranged",
+				levelName = "level1",
+				value = math.random(100), -- randomized for fun
+
+			}
+
+			analytics.logEvent("CoinsCollected",eventData); -- eg: we track coins collected in level1
+			analytics.forceSend();	
+		end
+							
+	-- DATA METRICS	( NOTE: you need to enable Data API in the Playtomic Dashboard. Settings-> Game Details -> Data API)
+		-- LOAD VIEWS
+		TEST.DataMetricsLoadViews = function()
+			-- trivial, hence delayed
+		end
+
+		-- LOAD PLAYS
+		TEST.DataMetricsLoadPlays = function()
+			-- trivial, hence delayed	
+		end
+
+		-- LOAD VIEWS
+		TEST.DataMetricsLoadPlayTime = function()
+			-- trivial, hence delayed	
+		end
+
+		-- LOAD CUSTOM METRIC. This loads all the metrics from Playtomic's servers to your app/game
+		TEST.DataMetricsLoadCustomMetric = function()
+			-- eg: we're loading the custom metrics as shown TEST.CustomMetric
+			analytics.Data.CustomMetric("SomeEventHappened",TEST.DataMetricsLoadCustomMetric_Callback)
+		end
+			-- CALLBACK FOR LOAD CUSTOM METRIC
+			TEST.DataMetricsLoadCustomMetric_Callback = function(data,response)
+				if response.Success then
+					print(data.Name .. " has value of " .. data.Value)
+				else
+					print("Error loading Custom Metric")
+				end
+			end
+
+		-- LOAD LEVEL COUNTER METRIC
+		TEST.DataMetricsLoadLevelCounterMetric = function()
+			analytics.Data.LevelCounterMetric("Win",TEST.DataMetricsLoadCounterMetric_Callback)
+		end
+			-- CALLBACK FOR LOAD COUNTER METRIC
+			TEST.DataMetricsLoadCounterMetric_Callback = function(data,response)
+				if response.Success then
+					print(data.Name .. " has value of " .. data.Value .. " on level " .. data.Level)
+				else
+					print("Error loading Level Counter Metric")
+				end
+			end
+				
+		-- LOAD LEVEL AVERAGE METRIC
+		TEST.DataMetricsLoadLevelAverageMetric = function()
+			-- same process for the above
+		end
+
+		-- LOAD LEVEL RANGED METRIC
+		TEST.DataMetricsLoadLevelRangedMetric = function()
+			-- same process for the above
+		end
+							
 	--[[ GAME VARS		
-		to set this up, you need to add a GameVar in your Playtomic dashboard
-		in our case, we added a GameVar called "CloudSpeed", and set it to an arbitrary integer, 2.
+		to set this up, you need to add a GameVar in your Playtomic dashboard (under Settings->Gamevars)
+		in our case, we added 5 different variables, called Variable1, Variable2, Variable3, Variable4, ArbitraryVariableName and assigned them different values
 		the LoadGameVars function attempts to retrieve that value from Playtomic's database, and insert it into the CoronaSDK game
 	]]--
 	TEST.LoadGameVars = function()
@@ -26,11 +140,13 @@ local TEST = {}
 	end
 	
 	TEST.GameVarsLoaded = function(vars,response)
-		    if (response.Success) then	    
+		    if (response.Success) then
 				-- print all GameVars
-				for k,v in vars do
-					print (k,v)
-				end
+				print("Variable1: " .. vars.Variable1)
+				print("Variable2: " .. vars.Variable2)
+				print("Variable3: " .. vars.Variable3)
+				print("Variable4: " .. vars.Variable4)
+				print("ArbitraryVariableName: " .. vars.ArbitraryVariableName)				
 		    else	    
 		        print('error loading GameVars')
 		    end
